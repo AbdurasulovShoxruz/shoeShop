78b24000d38c313f8020cecd07194789a2191c62
shoxvrz <shoxruzabdurasulov7@gmail.com>
2024-04-18 23:04:25 +0500

    gg

--- src/components/MediaCartModal/MediaCartModal.jsx
+++ src/components/MediaCartModal/MediaCartModal.jsx
@@ -1,13 +1,57 @@
-import React from 'react'
-import './MediaCartModal.scss'
+import React from "react";
+import "./MediaCartModal.scss";
 
-
-function MediaCartModal({ toggleHandler, addToCartHandler, cartItem, removeHandler, totalQuantity, totalPrice }) {
+function MediaCartModal({
+  toggleHandler,
+  addToCartHandler,
+  cartItem,
+  removeHandler,
+  totalQuantity,
+  totalPrice,
+}) {
   return (
-    <div className='mediaCartModal'>
-            <h1>sdafadsfdsaf</h1>
+    <div className="mediaCartModal">
+        <button onClick={toggleHandler} className="closeIcon">X</button>
+      {cartItem.length === 0 ? (
+        <p>Empty</p>
+      ) : (
+        cartItem.map((shoe) => (
+          <div key={shoe.id} className="mediaCartModal__shoe">
+            <div className="mediaCartModal__shoe--image">
+              <img src={shoe.image} alt="shoe Image" />
+            </div>
+            <div className="mediaCartModal__shoe--title">
+              <h1>{shoe.name}</h1>
+              <p>${shoe.price}</p>
+            </div>
+            <div className="mediaCartModal__shoe--buttons">
+              <button onClick={() => removeHandler(shoe)}>-</button>
+              <span>{shoe.qty}</span>
+              <button onClick={() => addToCartHandler(shoe)}>+</button>
+            </div>
+          </div>
+        ))
+      )}
+      <div className="mediaCartModal__calc">
+        <div>
+            <h1>Total Quantity</h1>
+            <p>{totalQuantity}</p>
+        </div>
+        <div>
+            <h1>Delivery Fee</h1>
+            <p>$10.0</p>
+        </div>
+        <div>
+            <h1>Sales Tax</h1>
+            <p>$5.0</p>
+        </div>
+        <div>
+            <h1>Total Price</h1>
+            <p>${totalPrice.toFixed(1)}</p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default MediaCartModal
+export default MediaCartModal;
